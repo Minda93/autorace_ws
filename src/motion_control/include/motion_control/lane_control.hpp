5174d8b254d0afb27c5db50041d6013fc035fac6
minda93 <wwe421210w@gmail.com>
2021-03-12 06:13:27 +0000

    Update motion_control struct
    Next step, design PID controller
    detect_lane need to normalize centerX

--- src/motion_control/include/motion_control/lane_control.hpp
+++ src/motion_control/include/motion_control/lane_control.hpp
@@ -1,6 +1,9 @@
 #ifndef MOTION_CONTROL_LANE_CONTROL_HPP_
 #define MOTION_CONTROL_LANE_CONTROL_HPP_
 
+#include "motion_control/motion_control_param.hpp"
+#include "motion_control/controller/pid.hpp"
+
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64.hpp"
 #include "geometry_msgs/msg/twist.hpp"
@@ -29,17 +32,26 @@ namespace motion_control
     void lane_callback(const std_msgs::msg::Float64::SharedPtr msg);
 
     // publisher
-    void pub();
+    void pub(const RobotVel &robotVel);
 
     // controller
-    void pid_controller();
+    RobotVel pid_controller(const double state);
 
   private:
     // topic
     rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr lane_center_sub_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
 
+    // Timer
+    rclcpp::Clock clock_;
+    rclcpp::Time prevTime_;
+    rclcpp::TimerBase::SharedPtr runTimer_;
     
+    LaneCtrlParam cfg_; // param
+    bool trigger_; 
+    double centerX_; // control error
+    std::shared_ptr<controller::PID> pid_ctrl_ptr_;
+
   }; // class LaneControl
   
 } // namespace motion_control
--- src/motion_control/src/lane_control.cpp
+++ src/motion_control/src/lane_control.cpp
@@ -7,11 +7,13 @@
 
 #include <vector>
 #include <functional>
+#include <chrono>
 
 namespace motion_control
 {
   LaneControl::LaneControl(const rclcpp::NodeOptions &options)
-    : Node("lane_control", options)
+    : Node("lane_control", options),
+    trigger_(false), centerX_{0.0}
   {
     // param
     parse_parameters();
@@ -29,12 +31,22 @@ namespace motion_control
     cmd_vel_pub_ = create_publisher<geometry_msgs::msg::Twist>(
       "cmd_vel", 10);
 
-    RCLCPP_INFO(this->get_logger(),"INIT");
+    // timer
+    clock_ = rclcpp::Clock(RCL_ROS_TIME);
+    prevTime_ = clock_.now();
+
+    // run rate 25 hz
+    runTimer_ = create_wall_timer(
+      std::chrono::milliseconds(40),
+      std::bind(&LaneControl::run, this));
+
+    // RCLCPP_INFO(this->get_logger(),"INIT");
   }
 
   void LaneControl::parse_parameters()
   {
-
+    pid_ctrl_ptr_ = std::make_shared<controller::PID>(
+      cfg_.ctrlTarget, cfg_.pid.kp, cfg_.pid.ki, cfg_.pid.kd);
   }
 
   rcl_interfaces::msg::SetParametersResult LaneControl::dynamic_load_params(
@@ -45,30 +57,61 @@ namespace motion_control
 
   void LaneControl::lane_callback(const std_msgs::msg::Float64::SharedPtr msg)
   {
+    centerX_ = msg->data;
+    trigger_ = true;
 
+    // RCLCPP_INFO(this->get_logger(),"centerX : %lf", centerX_);
   }
 
-  void pid_controller()
+  RobotVel LaneControl::pid_controller(const double state)
   {
-    
+    // time
+    rclcpp::Duration duration{clock_.now() - prevTime_};
+    double delta{duration.seconds()};
+
+    double pidValue{pid_ctrl_ptr_->run(state, delta)};
+    RobotVel robotVel{0.0, 0.0};
+
+    // design control
+
+    return robotVel;
   }
 
   void LaneControl::stop_control()
   {
-
+    pub(RobotVel{0.0, 0.0});
   }
 
-  void run()
+  void LaneControl::run()
+  { 
+    if(trigger_)
+    {
+      RobotVel robotVel{pid_controller(centerX_)};
+      pub(robotVel);
+    }
+    trigger_ = false;
+  }
+
+  void LaneControl::pub(const RobotVel &robotVel)
   {
+    auto twist = std::make_unique<geometry_msgs::msg::Twist>();
+    
+    if(robotVel.linearX > cfg_.maxLinearVel)
+      twist->linear.x = cfg_.maxLinearVel;
+    else if(robotVel.linearX < -cfg_.maxLinearVel)
+      twist->linear.x = -cfg_.maxLinearVel;
+    else
+      twist->linear.x = robotVel.linearX;
 
+    if(robotVel.angularZ > cfg_.maxAngularVel)
+      twist->angular.z = cfg_.maxAngularVel;
+    else if(robotVel.angularZ < -cfg_.maxAngularVel)
+      twist->angular.z = -cfg_.maxAngularVel;
+    else
+      twist->angular.z = robotVel.angularZ;
+
+    cmd_vel_pub_->publish(std::move(twist));
   }
-
-  void pub()
-  {
-
-  }
-
-  
 } // motion_control
 
 RCLCPP_COMPONENTS_REGISTER_NODE(motion_control::LaneControl)
